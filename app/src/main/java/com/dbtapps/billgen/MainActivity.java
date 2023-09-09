979bc36cb575acde5eb168e0f96e1643bab485fe
Debarghya Basak <debarghyabasakofficial.2001@gmail.com>
2023-09-09 15:01:51 +0530

    Base Theme added

--- app/src/main/java/com/dbtapps/billgen/MainActivity.java
+++ app/src/main/java/com/dbtapps/billgen/MainActivity.java
@@ -1,5 +1,13 @@
 package com.dbtapps.billgen;
 
+/*TO RUN ANDROID 13 EMULATOR (USING VULKAN DRIVER)......
+
+GO TO COMMAND LINE AND TYPE
+
+emulator -avd <name of the emulator> -feature -Vulkan
+*/
+
+
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
