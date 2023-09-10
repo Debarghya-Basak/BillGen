0a58c64774198a18fdea2c033cedda32493c7beb
Debarghya Basak <debarghyabasakofficial.2001@gmail.com>
2023-09-11 02:20:25 +0530

    bottom sheet dialog update

--- app/src/main/java/com/dbtapps/billgen/Activities/BillEditor.java
+++ app/src/main/java/com/dbtapps/billgen/Activities/BillEditor.java
@@ -2,12 +2,16 @@ package com.dbtapps.billgen.Activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Dialog;
 import android.os.Bundle;
+import android.view.View;
 import android.view.WindowManager;
+import android.widget.LinearLayout;
 
 import com.dbtapps.billgen.R;
 import com.dbtapps.billgen.databinding.ActivityBillEditorBinding;
 import com.google.android.material.bottomsheet.BottomSheetDialog;
+import com.google.android.material.button.MaterialButton;
 
 import DatabaseManager.FirebaseDatabaseConnections;
 
@@ -41,6 +45,26 @@ public class BillEditor extends AppCompatActivity {
         BottomSheetDialog bottomSheetDialog = new BottomSheetDialog(this);
         bottomSheetDialog.setContentView(R.layout.add_item_colorandsize_dialog);
         bottomSheetDialog.getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_ADJUST_RESIZE);
+
+        LinearLayout addItemAddColorOrSizeBtnContainerLl = bottomSheetDialog.findViewById(R.id.addItemAddColorOrSizeBtnContainerLl);
+        LinearLayout addItemContainerLl = bottomSheetDialog.findViewById(R.id.addItemContainerLl);
+        LinearLayout addColorOrSizeContainerLl = bottomSheetDialog.findViewById(R.id.addColorOrSizeContainerLl);
+        LinearLayout addCancelBtnContainerLl = bottomSheetDialog.findViewById(R.id.addCancelBtnContainerLl);
+        MaterialButton addItemBtn = bottomSheetDialog.findViewById(R.id.addItemBtn);
+        MaterialButton addColorOrSizeBtn = bottomSheetDialog.findViewById(R.id.addColorOrSizeBtn);
+
+        addItemBtn.setOnClickListener(v -> {
+            addItemAddColorOrSizeBtnContainerLl.setVisibility(View.GONE);
+            addItemContainerLl.setVisibility(View.VISIBLE);
+            addCancelBtnContainerLl.setVisibility(View.VISIBLE);
+        });
+
+        addColorOrSizeBtn.setOnClickListener(v -> {
+            addItemAddColorOrSizeBtnContainerLl.setVisibility(View.GONE);
+            addColorOrSizeContainerLl.setVisibility(View.VISIBLE);
+            addCancelBtnContainerLl.setVisibility(View.VISIBLE);
+        });
+
         bottomSheetDialog.show();
     }
 
